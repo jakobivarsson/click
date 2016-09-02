832c13751f84d540652e6b3b654b59654df7a044
jakobivarsson <jakob.ivarsson94@gmail.com>
2016-09-02 14:25:03 +0200

    removed old frontend

--- frontend/sw.js
+++
@@ -1,8 +0,0 @@
-importScripts('node_modules/sw-toolbox/sw-toolbox.js');
-
-toolbox.options.debug = false;
-
-toolbox.precache(['/']);
-// toolbox.router.get('/(.*)', toolbox.fastest);
-// toolbox.router.get('/(.*)', toolbox.fastest, { origin: 'https://fonts.googleapis.com/' });
-toolbox.router.default = toolbox.fastest;
