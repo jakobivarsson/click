510a4a89349e805e339388b82eacb83a2ab2de6a
jakobivarsson <jakob.ivarsson94@gmail.com>
2016-09-02 14:22:34 +0200

    added elm frontend

---
+++ qlicky/qlicky.elm
@@ -0,0 +1,124 @@
+import Html exposing (..)
+import Html.App as Html
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+import Json.Decode exposing (..)
+import Keyboard
+import List
+import Maybe
+import Platform.Sub
+import Result
+import String
+import Task
+import WebSocket
+
+
+main =
+  Html.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+server : String
+server =
+  "ws://localhost:8008/counters/Nymble"
+
+
+
+-- MODEL
+
+
+type alias Model =
+  { counter : Counter
+  , counters : List String
+  , authenticated : Bool
+  }
+
+type alias Counter = 
+  { name : String
+  , value : Int
+  }
+
+init : (Model, Cmd Msg)
+init =
+    (Model (Counter "Nymble" 0) [] True, getCounters)
+
+
+
+-- UPDATE
+
+type Msg
+  = Increment
+  | Decrement
+  | Update String
+  | GetCounters (List String)
+  | HttpError Http.Error
+  | NoOp
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Increment ->
+      (model, WebSocket.send server "increment")
+
+    Decrement ->
+      (model, WebSocket.send server "decrement")
+
+    Update value ->
+      ({model | counter = Counter model.counter.name (safeToInt model.counter.value value)}, Cmd.none)
+
+    GetCounters counters ->
+      ({model | counters = counters}, Cmd.none)
+    
+    HttpError error ->
+      ({model | counters = ["error"]}, Cmd.none)
+    
+    NoOp ->
+      (model, Cmd.none)
+
+safeToInt default = String.toInt >> Result.toMaybe >> Maybe.withDefault default
+
+getCounters =
+  Task.perform HttpError GetCounters (Http.get countersDecoder "http://localhost:8008/counters")
+
+countersDecoder =
+  Json.Decode.list Json.Decode.string
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Platform.Sub.batch 
+    [ WebSocket.listen server Update
+    , Keyboard.downs keyListener
+    ]
+
+keyListener key =
+  case key of
+    39 -> Increment
+    37 -> Decrement
+    _  -> NoOp
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ div [] [text model.counter.name]
+    , div [] [text (toString model.counter.value)]
+    , button [onClick Decrement] [text "-"]
+    , button [onClick Increment] [text "+"]
+    , ul [] (List.map (\c -> li [] [text c]) model.counters)
+    ]
+
+
