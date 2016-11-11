2d98f9eb4e50a03af1c1b6f2e0eae8b9e7b67762
Tiseno <math.lindgren@gmail.com>
2016-11-11 14:55:40 +0100

    hashing of username aswell

--- server/auth.go
+++ server/auth.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/sha256"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -14,12 +15,16 @@ func hash(password string) string {
 
 func CreateUser(user string, pass string) {
 	db := GetDB()
-	db.CreateAuthority(user, hash(pass))
+	sh := sha256.New()
+	sh.Write([]byte(user))
+	db.CreateAuthority(string(sh.Sum(nil)), hash(pass))
 }
 
 func CheckPassword(user string, pass string) bool {
 	db := GetDB()
-	if bcrypt.CompareHashAndPassword([]byte(db.Auth(user)), []byte(pass)) == nil {
+	sh := sha256.New()
+	sh.Write([]byte(user))
+	if bcrypt.CompareHashAndPassword([]byte(db.Auth(string(sh.Sum(nil)))), []byte(pass)) == nil {
 		return true
 	}
 	return false
