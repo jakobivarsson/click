853ef86d90471624ad31d02742f9632349900c83
Tiseno <math.lindgren@gmail.com>
2016-11-15 12:37:40 +0100

    created a unified CLI for click

---
+++ server/cmd/click/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/jakobivarsson/click/server/click"
+	"golang.org/x/crypto/ssh/terminal"
+	"os"
+	"strings"
+	"syscall"
+)
+
+func main() {
+	arg := os.Args[1:]
+	if len(arg) < 1 {
+		help()
+		return
+	}
+	switch arg[0] {
+	case "createuser":
+		createUser()
+	case "help":
+		help()
+	case "init":
+		initdb()
+	case "run":
+		run()
+	default:
+		fmt.Println("Invalid argument!")
+		help()
+	}
+}
+
+func createUser() {
+	DB := click.GetDB()
+	DB.Open()
+	defer DB.Close()
+	user, pass := credentials()
+	click.CreateUser(user, pass)
+	if click.AuthenticateUser(user, pass) {
+		fmt.Println("User authenticated.")
+	} else {
+		fmt.Println("User not authenticated.")
+	}
+}
+
+func help() {
+	//TODO
+	fmt.Println("Click Help")
+	fmt.Println("createuser")
+	fmt.Println("init")
+	fmt.Println("run")
+}
+
+func initdb() {
+	DB := click.GetDB()
+	DB.Open()
+	defer DB.Close()
+	DB.LogEntry("Nymble", "click", 0)
+	DB.LogEntry("Nymble", "count", 0)
+	DB.LogEntry("KTHB", "click", 0)
+	DB.LogEntry("KTHB", "count", 0)
+	DB.PrintToday("Nymble", "click")
+	DB.PrintToday("Nymble", "count")
+	DB.PrintToday("KTHB", "click")
+	DB.PrintToday("KTHB", "count")
+}
+
+func run() {
+	fmt.Println("Starting click server")
+	click.RunServer()
+}
+
+func credentials() (string, string) {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("username: ")
+	username, _ := reader.ReadString('\n')
+
+	fmt.Print("password: ")
+	bytePassword, err := terminal.ReadPassword(int(syscall.Stdin))
+	if err != nil {
+		fmt.Println("Error reading password", err)
+		return "", ""
+	}
+	fmt.Print("\n")
+	password := string(bytePassword)
+	return strings.TrimSpace(username), strings.TrimSpace(password)
+}
