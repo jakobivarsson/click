a3a671b3ae76179091a1b577ddd7ef6fb24bd2d1
Jeppes <jespersandstrom@gmail.com>
2016-11-18 20:29:21 +0100

    more css?

--- client/src/components/buildings/buildings.js
+++ client/src/components/buildings/buildings.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-import './buildings.css';
 import { getCounters, COUNTERS } from '../../messages';
 import { connect } from '../../auth';
 import { Link } from 'react-router';
-
+import './buildings.css';
 
 class Buildings extends Component {
   constructor(props) {
     super(props);
-    // localStorage.username = "";
-    // localStorage.password = "";
     this.state = {
       buildings: [],
       fetching: false
@@ -21,14 +18,14 @@ class Buildings extends Component {
     connect(ws => {
       ws.onmessage = event => {
         const message = JSON.parse(event.data)
-        if(message.type === COUNTERS) {
+        if (message.type === COUNTERS) {
           this.setState({
             buildings: message.counters.sort(),
             fetching: false
           });
         }
       }
-      ws.send(getCounters());	
+      ws.send(getCounters());
       this.setState({fetching: true});
     });
   }
@@ -41,14 +38,14 @@ class Buildings extends Component {
     return buildings.map(building =>
       <li key={building} className='building'>
         <Link to={`/buildings/${building}`}>{building}</Link>
-        </li>
+      </li>
     );
   }
 
   render() {
     const buildings = this.state.buildings;
     let list;
-    if(this.state.fetching) {
+    if (this.state.fetching) {
       list = <div className='loader buildings-loader' />;
     } else {
       list = <ul>{this.getBuildings(buildings)}</ul>
@@ -56,10 +53,11 @@ class Buildings extends Component {
     return (
       <div className='buildings-container'>
         <div className='buildings'>
-        <h1>Buildings</h1>
-        {list}
+          <h1>Buildings</h1>
+          {list}
+          <Link to="/statistics">Statistics</Link>
         </div>
-        </div>
+      </div>
     );
   }
 }
