971dec225a25e3bf2f4c45e440016393c0d50626
Michel Tabari <tabari@kth.se>
2018-02-03 19:41:33 +0100

    Center items

--- client/src/components/indicators/Indicator.js
+++ client/src/components/indicators/Indicator.js
@@ -7,9 +7,9 @@ import './Indicator.css'
 
 const Indicator = () => (
   <div className="spinner">
-    <div className="bounce1"></div>
-    <div className="bounce2"></div>
-    <div className="bounce3"></div>
+    <div className="bounce1"/>
+    <div className="bounce2"/>
+    <div className="bounce3"/>
   </div>
 )
 
